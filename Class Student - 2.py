43383dbeb39e6698b145ca243d69c182674aaa4d
ynawinraaj <gomsyuva19@gmail.com>
2024-10-10 20:50:26 +0530

    Add files via upload

---
+++ Class Student - 2.py
@@ -0,0 +1,13 @@
+class student:
+    grade=8
+    name="Raaj"
+    
+    def introdection(self):
+        print("Hi i am a student")
+    def detiles(self):
+        print("My name is",self.name)
+        print("I study in grade",self.grade)
+        
+ob=student()
+ob.introdection()
+ob.detiles()    
